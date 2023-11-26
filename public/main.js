61ad3f3bdd49231a59ed8e0e50b3339d291cfabb
Vejber <105496392+Vejber@users.noreply.github.com>
2023-11-26 15:37:01 +0300

    html css valid and script

---
+++ public/main.js
@@ -0,0 +1,34 @@
+// Обработчик события для ссылок
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("a");
+
+    links.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Предотвратить стандартное поведение ссылки
+
+            const targetPage = this.getAttribute("href");
+            navigateTo(targetPage);
+        });
+    });
+});
+
+// Функция для перехода на страницу
+function navigateTo(page) {
+    // Предположим, у вас есть функция makeApiRequest, которая возвращает Promise с объектом Response
+    makeApiRequest()
+        .then(response => {
+            // Проверяем код ответа
+            if (response.ok) {
+                // Успешный запрос, перенаправляем на указанную страницу
+                window.location.href = page;
+            } else {
+                // Неуспешный запрос, перенаправляем на страницу ошибки
+                window.location.href = "error.html";
+            }
+        })
+        .catch(error => {
+            // Обработка ошибок при запросе (например, сетевые проблемы)
+            console.error("Request error:", error);
+            window.location.href = "error.html"; // Перенаправляем на страницу ошибки в случае ошибки запроса
+        });
+}
